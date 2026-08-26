df885fcb325083de3607568e4b54fc1fdaf49a49
Eliã Silva dos Anjos e Silva <eliaanjos4@gmail.com>
2026-08-26 20:57:05 -0300

    Change primary key column from id_professor to id

--- ddl/professor.sql
+++ ddl/professor.sql
@@ -1,5 +1,5 @@
 CREATE TABLE professor (
-    id_professor SERIAL PRIMARY KEY,
+    id int SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
     telefone VARCHAR(15),
