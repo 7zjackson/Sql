58b52c4a32baaec35bfd37abfc8f93191a626a2f
camillyrbr <camilly.victoria.030313@gmail.com>
2026-08-26 19:48:25 -0300

    Atualizar o tabela arena.sql

--- tabela arena.sql
+++ tabela arena.sql
@@ -47,4 +47,15 @@ CREATE TABLE matricula (
     data_termino DATE,
     status VARCHAR(30)
 );
+CREATE TABLE mensalidade (
+    id_mensalidade SERIAL PRIMARY KEY,
+    mes_referencia VARCHAR(7),
+    data_vencimento DATE NOT NULL,
+    data_pagamento DATE,
+    valor NUMERIC(10, 2),
+    forma_pagamento VARCHAR(30),
+    status VARCHAR(30)
+);
+
+
 
