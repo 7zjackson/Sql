542694d58e8d5d4c1606b246d4c75630082f2b61
Caio669max <caio02766@gmail.com>
2026-09-08 17:55:03 -0300

    Atualizar o professor.sql

--- dml/professor.sql
+++ dml/professor.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO professor (nome, cpf, telefone, email, especialidade, data_contratacao, ativo) 
+VALUES ('Carlos Souza', '22222222222', '11888880001', 'carlos@email.com', 'Tênis', CURRENT_DATE, TRUE);
