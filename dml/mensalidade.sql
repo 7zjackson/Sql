523d43d883faf5076a42e339b6e9ed204557faf6
7zjackson <jacksonsouza20170@icloud.com>
2026-09-08 17:50:58 -0300

    Add initial insert statement for mensalidade table

--- dml/mensalidade.sql
+++ dml/mensalidade.sql
@@ -1,1 +1,3 @@
+INSERT INTO mensalidade (id_matricula, id_forma_pagamento, mes_referencia, data_vencimento, data_pagamento, valor, status) 
+VALUES (1, 1, '2023-01', '2023-01-15', '2023-01-10', 150.00, 'Pago');
 
