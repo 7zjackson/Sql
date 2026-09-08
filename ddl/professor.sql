7732eb038fadef9717e0229444e2dc4890774768
camillyrbr <camilly.victoria.030313@gmail.com>
2026-09-08 19:07:12 -0300

    Update professor.sql

--- ddl/professor.sql
+++ ddl/professor.sql
@@ -11,3 +11,13 @@ CREATE TABLE professor (
 
 DROP TABLE IF EXISTS professor CASCADE;
 
+CREATE TABLE professor (
+id_professor SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+cpf VARCHAR(11) NOT NULL UNIQUE,
+telefone VARCHAR(15),
+email VARCHAR(100) UNIQUE,
+especialidade VARCHAR(100),
+data_contratacao DATE NOT NULL,
+ativo BOOLEAN DEFAULT TRUE
+);
