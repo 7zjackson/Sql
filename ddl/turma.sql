dd1d2e55a062ad2dc6f5f9a92d3a6ceff15d376f
juliagsouza-png <juliagsouza18@gmail.com>
2026-09-08 17:38:50 -0300

    Criar turma.sql

---
+++ ddl/turma.sql
@@ -0,0 +1,13 @@
+CREATE TABLE turma (
+id_turma SERIAL PRIMARY KEY,
+id_professor INT NOT NULL,
+id_nivel INT NOT NULL,
+nome VARCHAR(50) NOT NULL,
+dia_semana VARCHAR(20) NOT NULL,
+horario_inicio TIME NOT NULL,
+horario_fim TIME NOT NULL,
+vagas_maximas INT NOT NULL,
+ativa BOOLEAN DEFAULT TRUE,
+FOREIGN KEY (id_professor) REFERENCES professor(id_professor) ON DELETE CASCADE,
+FOREIGN KEY (id_nivel) REFERENCES nivel_turma(id_nivel) ON DELETE CASCADE
+);
