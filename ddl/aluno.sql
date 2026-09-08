29b0dd9b3d90b8789afc440a7aea12b5971f3cfd
camillyrbr <camilly.victoria.030313@gmail.com>
2026-09-08 17:53:27 -0300

    Atualizar o aluno.sql

--- ddl/aluno.sql
+++ ddl/aluno.sql
@@ -11,6 +11,16 @@ CREATE TABLE aluno (
 );
 
 DROP TABLE IF EXISTS aluno CASCADE;
-
+CREATE TABLE aluno (
+id_aluno SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+cpf VARCHAR(11) NOT NULL UNIQUE,
+data_nascimento DATE,
+sexo VARCHAR(20),
+telefone VARCHAR(15),
+email VARCHAR(100) UNIQUE,
+data_cadastro DATE NOT NULL,
+ativo BOOLEAN DEFAULT TRUE
+);
 
 
