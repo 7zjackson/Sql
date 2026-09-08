3b008010d5847113d6da9d298176ad5d68d56f56
juliagsouza-png <juliagsouza18@gmail.com>
2026-09-08 17:39:34 -0300

    Atualizar o mensalidade.sql

--- ddl/mensalidade.sql
+++ ddl/mensalidade.sql
@@ -9,3 +9,16 @@ CREATE TABLE mensalidade (
 );
 
 DROP TABLE IF EXISTS mensalidade CASCADE;
+
+CREATE TABLE mensalidade (
+id_mensalidade SERIAL PRIMARY KEY,
+id_matricula INT NOT NULL,
+id_forma_pagamento INT NOT NULL,
+mes_referencia VARCHAR(7) NOT NULL,
+data_vencimento DATE NOT NULL,
+data_pagamento DATE,
+valor NUMERIC(10, 2) NOT NULL,
+status VARCHAR(30) DEFAULT 'Pendente',
+FOREIGN KEY (id_matricula) REFERENCES matricula(id_matricula) ON DELETE CASCADE,
+FOREIGN KEY (id_forma_pagamento) REFERENCES forma_pagamento(id_forma_pagamento) ON DELETE RESTRICT
+);
