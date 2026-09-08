bcb803f7b320615873b75a33cccaab837291f110
7zjackson <jacksonsouza20170@icloud.com>
2026-09-08 17:49:53 -0300

    Add insert statement for locacao table

--- dml/locacao.sql
+++ dml/locacao.sql
@@ -1,1 +1,3 @@
+INSERT INTO locacao (id_aluno, id_quadra, id_forma_pagamento, data_locacao, hora_inicio, hora_fim, valor_total, status) 
+VALUES (1, 1, 1, '2023-02-01', '18:00', '19:00', 100.00, 'Finalizada');
 
