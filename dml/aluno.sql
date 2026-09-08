4b170f27174078b23414a978747bd578dd6a7bd8
Caio669max <caio02766@gmail.com>
2026-09-08 17:54:24 -0300

    Atualizar o aluno.sql

--- dml/aluno.sql
+++ dml/aluno.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO aluno (nome, cpf, data_nascimento, sexo, telefone, email, data_cadastro, ativo) 
+VALUES ('João Silva', '11111111111', '1990-01-01', 'M', '11999990001', 'joao@email.com', CURRENT_DATE, TRUE);
