0ca473c4b2e49c98f46d8ac7bbaf9c2eb57130ac
juliagsouza-png <juliagsouza18@gmail.com>
2026-09-09 19:59:20 -0300

    Modify 'quadra' table structure and primary key
    Updated the 'quadra' table structure by changing the primary key column name from 'id_quadra' to 'id' and added default values for 'coberta' and 'iluminacao'.

--- ddl/quadra.sql
+++ ddl/quadra.sql
@@ -1,16 +1,7 @@
-CREATE TABLE quadra (
-    id int SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    coberta BOOLEAN,
-    iluminacao BOOLEAN,
-    valor_hora NUMERIC(10, 2),
-    ativa BOOLEAN
-);
-
 DROP TABLE IF EXISTS quadra CASCADE;
 
 CREATE TABLE quadra (
-id_quadra SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 nome VARCHAR(50) NOT NULL,
 coberta BOOLEAN DEFAULT FALSE,
 iluminacao BOOLEAN DEFAULT FALSE,
