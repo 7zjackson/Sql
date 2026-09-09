8a2b377161a46303a5aa45229f0d43c4fb588213
Eliã Silva dos Anjos e Silva <eliaanjos4@gmail.com>
2026-09-09 19:53:40 -0300

    Update aluno.sql

--- ddl/aluno.sql
+++ ddl/aluno.sql
@@ -1,19 +1,7 @@
-CREATE TABLE aluno (
-    id int SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    data_nascimento DATE,
-    sexo VARCHAR(20),
-    telefone VARCHAR(15),
-    email VARCHAR(100),
-    data_cadastro DATE,
-    ativo BOOLEAN
-);
-
 DROP TABLE IF EXISTS aluno CASCADE;
 
 CREATE TABLE aluno (
-id_aluno SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 nome VARCHAR(100) NOT NULL,
 cpf VARCHAR(11) NOT NULL UNIQUE,
 data_nascimento DATE,
