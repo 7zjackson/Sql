6cc31a5683bf47ba0c36031b944448db60044b59
juliagsouza-png <juliagsouza18@gmail.com>
2026-09-09 19:58:59 -0300

    Refactor professor table structure
    Updated the professor table structure by dropping the old table and creating a new one with modified column names.

--- ddl/professor.sql
+++ ddl/professor.sql
@@ -1,18 +1,7 @@
-CREATE TABLE professor (
-    id int SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    telefone VARCHAR(15),
-    email VARCHAR(100),
-    especialidade VARCHAR(100),
-    data_contratacao DATE,
-    ativo BOOLEAN
-);
-
 DROP TABLE IF EXISTS professor CASCADE;
 
 CREATE TABLE professor (
-id_professor SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 nome VARCHAR(100) NOT NULL,
 cpf VARCHAR(11) NOT NULL UNIQUE,
 telefone VARCHAR(15),
