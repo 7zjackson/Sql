8249f87f73da1f6a1836c9b9513e99a0802778f4
juliagsouza-png <juliagsouza18@gmail.com>
2026-09-09 20:17:51 -0300

    Modify foreign key references in locacao.sql
    Updated foreign key references in locacao.sql to use 'id' instead of specific columns.

--- ddl/locacao.sql
+++ ddl/locacao.sql
@@ -10,7 +10,7 @@ hora_inicio TIME NOT NULL,
 hora_fim TIME NOT NULL,
 valor_total NUMERIC(10, 2) NOT NULL,
 status VARCHAR(30) DEFAULT 'Confirmada',
-FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno) ON DELETE CASCADE,
-FOREIGN KEY (id_quadra) REFERENCES quadra(id_quadra) ON DELETE CASCADE,
-FOREIGN KEY (id_forma_pagamento) REFERENCES forma_pagamento(id_forma_pagamento) ON DELETE RESTRICT
+FOREIGN KEY (id) REFERENCES aluno(id) ON DELETE CASCADE,
+FOREIGN KEY (id) REFERENCES quadra(id) ON DELETE CASCADE,
+FOREIGN KEY (id) REFERENCES forma_pagamento(id)ON DELETE RESTRICT
 );
