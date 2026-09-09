c61afbec4635291457a13edfe48df42455f84033
juliagsouza-png <juliagsouza18@gmail.com>
2026-09-09 19:58:06 -0300

    Refactor plano table structure and primary key
    Removed the old table definition and updated the primary key column name.

--- ddl/plano.sql
+++ ddl/plano.sql
@@ -1,17 +1,7 @@
-CREATE TABLE plano (
-    id int SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(200),
-    frequencia_semanal INT,
-    duracao_meses INT,
-    valor NUMERIC(10, 2),
-    ativo BOOLEAN
-);
-
 DROP TABLE IF EXISTS plano CASCADE;
 
 CREATE TABLE plano (
-id_plano SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 nome VARCHAR(50) NOT NULL,
 descricao VARCHAR(200),
 frequencia_semanal INT NOT NULL,
