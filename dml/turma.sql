eab66effc1a53b8cb23d6e196b3637047f540950
7zjackson <jacksonsouza20170@icloud.com>
2026-09-09 20:24:14 -0300

    Update turma.sql

--- dml/turma.sql
+++ dml/turma.sql
@@ -1,2 +1,2 @@
-INSERT INTO turma (id_professor, id_nivel, nome, dia_semana, horario_inicio, horario_fim, vagas_maximas, ativa) 
+INSERT INTO turma (professor, nivel, nome, dia_semana, horario_inicio, horario_fim, vagas_maximas, ativa) 
 VALUES (1, 1, 'Turma A', 'Segunda', '08:00', '09:00', 10, TRUE);
